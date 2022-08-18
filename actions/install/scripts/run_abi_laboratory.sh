1fb9af298998ef5df83f09ff080d53f886b307c1
Vanessasaurus <814322+vsoch@users.noreply.github.com>
2022-08-17 18:52:02 -0600

    Add/fedora install action (#37)
    * adding fedora install pipeline
    * update ld library path and ensure dumper run in same directory
    
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- actions/install/scripts/run_abi_laboratory.sh
+++ actions/install/scripts/run_abi_laboratory.sh
@@ -14,6 +14,9 @@ printf "old: $old\n"
 printf "new: $new\n"
 printf "report: $report_path\n"
 
+dira=$(dirname ${old})
+dirb=$(dirname ${new})
+export LD_LIBRARY_PATH="${dira}:${dirb}:${LD_LIBRARY_PATH}"
 
 # cleanup if no custom report path is provided
 if [ -z "${report_path+xxx}" ]; then 
@@ -28,8 +31,13 @@ dump_old=$(mktemp /tmp/ABI-1-XXXXX.dump)
 dump_new=$(mktemp /tmp/ABI-2-XXXXX.dump)
 
 # Options: https://github.com/lvc/abi-dumper/blob/master/abi-dumper.pl#L112
+cd $dira
 abi-dumper $old -o $dump_old -lver 1
+cd -
+
+cd $dirb
 abi-dumper $new -o $dump_new -lver 2
+cd -
 
 # Options: https://github.com/lvc/abi-compliance-checker/blob/master/abi-compliance-checker.pl#L119
 abi-compliance-checker -l $name -old $dump_old -new $dump_new -report-path $report_path
